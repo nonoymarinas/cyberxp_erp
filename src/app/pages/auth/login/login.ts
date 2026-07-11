2f174620e194cf3e7956589fd0a31781f16957d3
FELICIANO JR MARINAS <nonoymarinas@live.com>
2026-07-12 04:48:17 +0800

    m

--- src/app/pages/auth/login/login.ts
+++ src/app/pages/auth/login/login.ts
@@ -1,27 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
-import {
-  ButtonLogin,
-  CxpInput,
-  IconCyberxp,
-  IconEye,
-  IconPassword,
-  IconUser,
-} from 'cyberxp-ui';
+import { ButtonLogin, CxpInput, IconCyberxp, IconEye, IconPassword, IconUser } from 'cyberxp-ui';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [
-    RouterLink,
-    ButtonLogin,
-    CxpInput,
-    IconCyberxp,
-    IconUser,
-    IconPassword,
-    IconEye,
-  ],
+  imports: [RouterLink, ButtonLogin, CxpInput, IconCyberxp, IconUser, IconPassword, IconEye],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
