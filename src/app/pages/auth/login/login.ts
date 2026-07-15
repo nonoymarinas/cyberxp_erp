4926eb64d14838fdaa2113a6b7f3e9a46ba184d1
speedx-jay <jomariejaybatingal@gmail.com>
2026-07-15 14:11:30 +0800

    Add form and log payload to console

--- src/app/pages/auth/login/login.ts
+++ src/app/pages/auth/login/login.ts
@@ -1,18 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { Router } from '@angular/router';
-
-import {CxpButtonLogin, CxpInputText, CxpIconCyberxpApp, CxpIconEyePassword } from 'cyberxp-ui';
+import { CxpButtonLogin, CxpInputText, CxpIconCyberxpApp, CxpIconEyePassword } from 'cyberxp-ui';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, CxpButtonLogin, CxpInputText, CxpIconCyberxpApp, CxpIconEyePassword],
+  imports: [RouterLink, CxpButtonLogin, CxpInputText, CxpIconCyberxpApp, CxpIconEyePassword, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  readonly fb = inject(FormBuilder);
+  readonly isSaving = signal(false);
   passwordVisible = false;
+
+  form: FormGroup = this.fb.group({
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+  });
 
   get passwordHidden(): boolean {
     return !this.passwordVisible;
@@ -22,12 +29,12 @@ export class Login {
     this.passwordVisible = !this.passwordVisible;
   }
 
- constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
   signIn() {
 
-    // Later you will validate the username/password.
+    console.log('payload:', this.form.getRawValue());
 
-    this.router.navigate(['/app']);
+    // this.isSaving.set(true);
   }
 }
